c412085cfb46f347d70dced1c0950888213512a5
ys305751572 <305751572@qq.com>
2016-03-16 18:04:52 +0800

    Rejected commit: Default

--- src/main/java/com/leoman/controller/weixin/WeixinBaseController.java
+++ src/main/java/com/leoman/controller/weixin/WeixinBaseController.java
@@ -1,5 +1,6 @@
 package com.leoman.controller.weixin;
 
+import com.leoman.entity.KUser;
 import com.leoman.service.KUserService;
 import com.leoman.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,19 +21,24 @@ public class WeixinBaseController {
     @Autowired
     private LoginService service;
 
+    @Autowired
+    private KUserService userService;
+
     public String register(String mobile, String code) {
 
-
+        KUser user = new KUser();
+        user.setMobile(mobile);
+        userService.register(user);
         return null;
     }
 
     public String login(HttpServletRequest request, HttpServletResponse response, String mobile, String password, Model model) {
         Boolean success = service.loginWeixin(request, mobile, password);
         if(success) {
-
+            return "";
         }
-
-        return null;
+        model.addAttribute("error","用户名密码错误");
+        return "";
     }
 
     public void sendCode() {
